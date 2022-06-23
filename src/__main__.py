1960b1552ecc2a28f320a54ed868583e943e3941
Wolfremium13 <wolfremiuminformatica@gmail.com>
2022-06-23 22:11:47 +0100

    ✨ Md to XML it's working

--- src/__main__.py
+++ src/__main__.py
@@ -1,12 +1,20 @@
 
+
+from src.md_to_xml import MarkdownToXML
 from src.xml_to_md import XMLToMarkdown
 
 
 if __name__ == "__main__":
-    print("Main")
-    # xml_path = './data/updated-xml-data.xml'
-    # markdown_parsed_content = XMLToMarkdown().parse(xml_path)
+    # Markdown to XML
+    md_folder_path = "./data/podcasts/ni-cero-ni-uno/episodios/"
+    xml_template_path = "./data/parse_to_xml_template.xml"
+    xml = MarkdownToXML().parse(md_folder_path, xml_template_path)
+    xml.write("./data/template_converted.xml", encoding="utf-8")
 
-    # output_file_name = 'markdown-converted'
-    # with open(f"./data/{output_file_name}.md", "w") as text_file:
-    #     text_file.write(markdown_parsed_content)
+    # XML to Markdown function
+    xml_path = './data/updated-xml-data.xml'
+    markdown_parsed_content = XMLToMarkdown().parse(xml_path)
+
+    output_file_name = 'markdown-converted'
+    with open(f"./data/{output_file_name}.md", "w") as text_file:
+        text_file.write(markdown_parsed_content)
---
+++ src/md_to_xml.py
@@ -0,0 +1,76 @@
+import os
+import xml.etree.ElementTree as ET
+from markdown import Markdown
+
+from src.episode import Episode
+
+
+class MarkdownToXML:
+
+    def parse(self, folder_path: str, xml_template_path: str) -> ET.ElementTree:
+        md_contents = self._read_files(folder_path)
+        episodies = map(self._parse_text_to_episode, md_contents)
+        return self._parse_xml_content(episodies, xml_template_path)
+
+    def _read_files(self, folder_path: str) -> str:
+        all_folder_files = os.listdir(folder_path)
+        md_file_names = [f for f in all_folder_files if f
+                         != "episodios.md"]  # Duplicated content
+        md_paths = [f"{folder_path}{file_name}" for file_name in md_file_names]
+        return map(self._extract_file_content, md_paths)
+
+    def _extract_file_content(self, md_full_path: str) -> str:
+        with open(md_full_path, 'r') as file:
+            return file.read()
+
+    def _parse_text_to_episode(self, content: str) -> Episode:
+        md = Markdown(extensions=['meta'])
+        html_content = md.convert(content)
+        metadata = md.Meta
+        last_phrase_after_description = "\n<h2>template: templates/episode.html</h2>\n"
+        description = html_content.split(last_phrase_after_description)[1]
+        episode = Episode(
+            title=metadata.get('title')[0].replace("\"", ""),
+            link=(
+                f"https://savvily.es/podcasts/ni-cero-ni-uno"
+                f"/episodios{metadata.get('slug')[0]}/"),
+            date=metadata.get('publishedtext')[0],
+            description=description,
+            link_mp3=metadata.get('audiolink')[0]
+        )
+        return episode
+
+    def _parse_xml_content(self, episodies, xml_template_path: str) -> str:
+        # Watch out string are literals, take care with indentations
+        self._register_all_namespaces(xml_template_path)
+        template = ET.parse(xml_template_path)
+        rss_tag = template.getroot()
+        channel_tag = rss_tag[0]
+        for episode in episodies:
+            item = ET.fromstring("<item></item>")
+            title = ET.fromstring(f"<title>{episode.title}</title>")
+            item.append(title)
+            link = ET.fromstring(f"<link>{episode.link}</link>")
+            item.append(link)
+            date = ET.fromstring(f"<pubDate>{episode.date}</pubDate>")
+            item.append(date)
+            guid = ET.fromstring(f"<guid isPermaLink=\"false\">{episode.link}</guid>")
+            item.append(guid)
+            enclosure = ET.fromstring(
+                f"<enclosure url=\"{episode.link_mp3}\" type=\"audio/mpeg\"></enclosure>")
+            item.append(enclosure)
+            description = ET.fromstring(f"<description></description>")
+            description.text = f"<![CDATA[{episode.description}]]>"
+            item.append(description)
+            duration = ET.Element("{http://www.itunes.com/dtds/podcast-1.0.dtd}duration")
+            duration.text = "0:00"
+            item.append(duration)
+            channel_tag.append(item)
+        ET.indent(template, space="\t", level=0)
+        return template
+
+    def _register_all_namespaces(self, xml_template_path: str):
+        namespaces = dict([node for _, node in ET.iterparse(
+            xml_template_path, events=['start-ns'])])
+        for ns in namespaces:
+            ET.register_namespace(ns, namespaces[ns])
